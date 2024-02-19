cb94fd0fb8c7d6658a99c9a34269c5aeb74a4447
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-02-19 10:47:16 +0530

    called api using fetch and promise

--- api-call-example/script.js
+++ api-call-example/script.js
@@ -1,30 +1,46 @@
-const postListContainer=document.querySelector('.posts-list-container');
+const postListContainer = document.querySelector(".posts-list-container");
 
-function fetchUsingXHR()
-{
-    const xhr=new XMLHttpRequest();
-    xhr.open('GET','https://jsonplaceholder.typicode.com/posts')
-    xhr.responseType='json';
-    xhr.send();
+function fetchUsingXHR() {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+  xhr.responseType = "json";
+  xhr.send();
 
-    console.log(xhr);
+  console.log(xhr);
 
-    xhr.onload=()=>{
-        if(xhr.status===200){
-            console.log(xhr);
-            displayResults(xhr.response)
-    } else{
-        console.log("error occured")
-         }
+  xhr.onload = () => {
+    if (xhr.status === 200) {
+      console.log(xhr);
+      displayResults(xhr.response);
+    } else {
+      console.log("error occured");
     }
+  };
 }
 
-function displayResults(posts){
-    postListContainer.innerHTML=posts.map((postItem)=>`
+function displayResults(posts) {
+  postListContainer.innerHTML = posts
+    .map(
+      (postItem) => `
     <div class="post-item">
     <h3>${postItem.title}</h3>
     <p>${postItem.body}</p>
     </div>
-    `).join("")
+    `
+    )
+    .join("");
 }
-fetchUsingXHR();
+//fetchUsingXHR();
+
+function fetchUsingFetchMethod() {
+  const fetchRequest = fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "GET",
+  });
+
+  fetchRequest
+    .then((response) => response.json())
+    .then((result) => displayResults(result))
+    .catch((e) => console.log(e));
+}
+
+fetchUsingFetchMethod();
