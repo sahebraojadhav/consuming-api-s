a7b721c827ac448321e1d08e9e957ecdb6b9428f
sahebrao jadhav <onlysahebpatil@gmail.com>
2024-02-19 11:50:05 +0530

    fetchUsingXHRAsyncAwait

--- api-call-example/script.js
+++ api-call-example/script.js
@@ -43,4 +43,41 @@ function fetchUsingFetchMethod() {
     .catch((e) => console.log(e));
 }
 
-fetchUsingFetchMethod();
+//fetchUsingFetchMethod();
+
+async function fetchUsingAsyncAwait(){
+    const response=await fetch("https://jsonplaceholder.typicode.com/posts",{
+      method:"GET"
+    })
+    const result=await response.json();
+
+    displayResults(result);
+}
+//fetchUsingAsyncAwait();
+
+function helperMethod(method,url){
+  const promise=new promise((resolve,reject)=>{
+        const xhr=new XMLHttpRequest();
+        xhr.open(method,url);
+        xhr.responseType="json";
+        xhr.send();
+
+        xhr.onload=()=>{
+          if(xhr.status==200){
+            resolve(xhr.response)
+          }
+          else{
+            reject(xhr.response)
+          }
+        }
+  })
+  return promise;
+}
+
+async function fetchUsingXHRAsyncAwait(){
+    const response=await helperMethod('GET',"https://jsonplaceholder.typicode.com/posts")
+    displayResults(response);
+
+  }
+
+fetchUsingXHRAsyncAwait()
